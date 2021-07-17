b94ec186b7ef94a97e0b73ff4e37b2add455a6f9
WenShuang Lu <wenshuang_lu@163.com>
2021-07-17 18:04:14 +0800

    charts: change clickhouse version to 0.14.0

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-db_name=postgresql-ha
-db_version=6.9.1
+#db_name=postgresql-ha
+#db_version=6.9.1
+
+#db_name=clickhouse-cluster
+#db_version=0.14.0
+
+db_name=clickhouse-operator
+db_version=0.14.0
 
 repo=https://dbkernel.github.io/helm-charts/
 
 set -x
 
+rm -f ./docs/$db_name*
 helm package charts/${db_name}
-helm repo index --url $repo ./docs
 mv ${db_name}-${db_version}.tgz ./docs
+if [ "${db_name}" == "clickhouse-operator" ]; then
+    cp charts/clickhouse-operator-install.yaml docs
+fi
+helm repo index --url $repo ./docs
 
 set +x
